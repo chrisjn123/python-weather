ade93c9bc2751195e18d744840391d80c12c798e
Chris Nickell <chrisjnickell@gmail.com>
2024-06-06 20:47:54 -0600

    added file locking for rw protection

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+'''
+Module to receive and connect to a UDP stream for Weather from ESP 32
+'''
+
 import socket
+from os import remove
 
 UDP_IP = ""
 UDP_PORT = 4210
@@ -6,12 +11,27 @@ UDP_PORT = 4210
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
-#sock.settimeout(5)
+
+count = 0
+
+def write_data(data: bytes) -> None:
+    with open('~/.weather.lock', 'w') as lock, open('~/.weather.dat', 'w') as w_data:
+        lock.write('Locking\n')
+        string_data = data.decode()
+        humidity, temp_f, pressure_hpa = string_data.split(';')
+        w_data.write(f'{humidity}\n')
+        w_data.write(f'{temp_f}\n')
+        w_data.write(f'{pressure_hpa}\n')
+    
+    remove('`/.weather.lock')
 
 def parser(d: bytes) -> None:
+    global count 
+    count += 1
     string_data = d.decode()
     humidity, temp_f, pressure_hpa = string_data.split(';')
 
+    print(f'Packet Count: {count}')
     print('Temperature: {:.2f}°F'.format(float(temp_f)))
     print('Humidity   : {:.2f}%'.format(float(humidity)))
     print('Pressure   : {:.2f}hPa\n'.format(float(pressure_hpa)))
@@ -28,6 +48,7 @@ try:
             sock.sendto(b'ack', addr)
         else:
             parser(data)
+            write_data(data)
 except TimeoutError as e:
     print(f'Socket Timed out: {e}')
 except KeyboardInterrupt:
